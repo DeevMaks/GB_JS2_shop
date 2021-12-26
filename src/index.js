0706318ae09f8a8ea8dc1fd6badbb0886b1ef699
Allpion <slavkin.131@gmail.com>
2021-12-26 14:50:21 +1000

    lesson 1 added css, scss, optimizations and server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import getProductList from "./mock/data.js";
 import renderGoodsList from "./showcase.js";
+import './styles/style.css'
+import './styles/style.scss'
 
 const productList = getProductList(20);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,44 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const optimization = () => {
+  const config = {}
+  if (isProd) {
+    config.minimizer = [
+      new CssMinimizerPlugin(),
+      new TerserWebpackPlugin()
+    ]
+    }
+  return config
+}
+
+const filename = ext => {
+  if (isDev) {
+    return `[name].${ext}`
+  }
+  return `[name].[hash].${ext}`
+}
 
 module.exports = {
+    mode: 'development',
     entry: {
         main: path.resolve(__dirname, './src/index.js'),
     },
 
     output: {
       path: path.resolve(__dirname, './dist'),
-      filename: '[name].bundle.js',
+      filename: filename(ext='js'),
     },
+
+    optimization: optimization(),
 
     plugins: [
       new HtmlWebpackPlugin({
@@ -17,5 +46,40 @@ module.exports = {
           template: path.resolve(__dirname, './public/template.html'), // шаблон
           filename: 'index.html', // название выходного файла
       }),
+      new MiniCssExtractPlugin({
+        filename: filename(ext='css')
+      }),
+      new CleanWebpackPlugin()
     ],
+
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {},
+            },
+            'css-loader',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {},
+            },
+            'css-loader'
+          ]
+        },
+      ]
+    },
+
+    devServer: {
+      port: 4200,
+      hot: isDev
+    },
 }
