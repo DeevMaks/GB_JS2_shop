80d03251a5305958f8045363032998eaa6847b07
Valeriy <76444329+Gagarin-valantine@users.noreply.github.com>
2021-12-26 19:33:35 +0300

    dz1

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,30 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/index.js'),
-    },
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
 
-    output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: '[name].bundle.js',
-    },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
 
-    plugins: [
-      new HtmlWebpackPlugin({
-          title: 'GeekBrains Shop JS2',
-          template: path.resolve(__dirname, './public/template.html'), // шаблон
-          filename: 'index.html', // название выходного файла
-      }),
-    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'GeekBrains Shop JS2',
+      template: path.resolve(__dirname, './public/template.html'), // шаблон
+      filename: 'index.html', // название выходного файла
+    }),
+  ],
+  plugins: [
+    new CopyWebpackPlugin({
+      title: 'GeekBrains Shop JS2',
+      template: path.resolve(__dirname, './public/template.css'), // шаблон
+      filename: 'style.css', // название выходного файла
+    }),
+  ],
 }
