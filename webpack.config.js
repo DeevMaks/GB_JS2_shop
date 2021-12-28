b7230632cb00bcf2988e7782a8acd5d52c037311
anlesem <anlesem@mail.ru>
2021-12-28 20:43:18 +0300

    ДЗ1. Настройка проекта.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,59 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+// Базовая настройка для корректной работы с путями
+const path = require('path');
+
+// Базовый плагин необходимый для вставки названия итоговых JS-файлов в HTML
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// Плагин очищающий от мусора папку dist
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// Плагин для переноса файлов в папку dist  
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/index.js'),
-    },
+	// Установка режима сборки по умолчанию, для избежания предупреждения. 
+	// Доступны два варианта 'development' (обычный) или 'production' (minimal).
+	mode: 'development',
 
-    output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: '[name].bundle.js',
-    },
+	// Точка входа. Точек входа может быть несколько (параллельные скрипты).
+	entry: {
+		main: path.resolve(__dirname, './src/index.js'),
+	},
 
-    plugins: [
-      new HtmlWebpackPlugin({
-          title: 'GeekBrains Shop JS2',
-          template: path.resolve(__dirname, './public/template.html'), // шаблон
-          filename: 'index.html', // название выходного файла
-      }),
-    ],
+	// Точка сборки 
+	output: {
+	path: path.resolve(__dirname, './dist'),
+	filename: '[name].bundle.js',
+	},
+
+	// Подключение для непрерывного отслеживания изменений
+	devServer: {
+		port: 4200
+	},
+
+	plugins: [
+		// Название итоговых JS-файлов в HTML. Настройка.
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, './public/template.html'), // шаблон
+			filename: 'index.html', // название выходного файла
+			minify: {
+				collapseWhitespace: true,
+			}
+		}),
+		// Очищение от мусора папки dist
+		new CleanWebpackPlugin(),
+		// Перенос файлов в папку dist
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, './public/css'),
+					to: path.resolve(__dirname, 'dist/css/')
+				},
+				{
+					from: path.resolve(__dirname, './public/img'),
+					to: path.resolve(__dirname, 'dist/img/')
+				}
+			]
+		}),
+	],
 }
