aa94a4d99f41969302b78b3e6e4f2b3f1dc079b6
John <chessmaster_87@mail.ru>
2021-12-28 21:52:38 +0500

    added dev-server and css creater from webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import getProductList from "./mock/data.js";
 import renderGoodsList from "./showcase.js";
+import "../public/styles/style.sass";
 
-const productList = getProductList(20);
+const productList = getProductList(22);
 
 renderGoodsList(productList);
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,41 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/index.js'),
-    },
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
 
-    output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: '[name].bundle.js',
-    },
-
-    plugins: [
-      new HtmlWebpackPlugin({
-          title: 'GeekBrains Shop JS2',
-          template: path.resolve(__dirname, './public/template.html'), // шаблон
-          filename: 'index.html', // название выходного файла
-      }),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'GeekBrains Shop JS2',
+      template: path.resolve(__dirname, './public/template.html'), // шаблон
+      filename: 'index.html', // название выходного файла
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Extracts CSS into separate files
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
     ],
+  },
+  devServer: {
+    port: 4200
+  },
 }
