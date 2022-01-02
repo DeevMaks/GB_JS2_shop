af561d86e724a06d122560ab56fcafea7711ee24
Allpion <slavkin.131@gmail.com>
2022-01-02 18:36:30 +1000

    Lesson 2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import getProductList from "./mock/data.js";
 import renderGoodsList from "./showcase.js";
+import { send } from "./utils.js";
 import './styles/style.css'
 import './styles/style.scss'
 
-const productList = getProductList(20);
+const API_URL = 'http://localhost:3000/api/v1'
+let productList = [];
+let cart = [];
 
-renderGoodsList(productList);
+send((error) => { console.log(err) }, (res) => {
+    let list = JSON.parse(res);
+    productList = list;
+    renderGoodsList(productList);
+}, `${API_URL}/catalog`)
+
+let buyed = {id: 5, title: "new", price: 999};
+send((error) => { console.log(err) }, (res) => {
+    cart.push(buyed)
+}, `${API_URL}/cart`, 'POST', JSON.stringify(buyed), {"Content-Type": "application/json"})
