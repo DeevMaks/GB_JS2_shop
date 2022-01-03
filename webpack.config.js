59201ab902dcb654df70d330625cea5c8db1f9c6
Valeriy <76444329+Gagarin-valantine@users.noreply.github.com>
2022-01-03 21:22:33 +0300

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -10,7 +9,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.js',
-
   },
 
   plugins: [
@@ -20,11 +18,27 @@ module.exports = {
       filename: 'index.html', // название выходного файла
     }),
   ],
-  plugins: [
-    new CopyWebpackPlugin({
-      title: 'GeekBrains Shop JS2',
-      template: path.resolve(__dirname, './public/template.css'), // шаблон
-      filename: 'style.css', // название выходного файла
-    }),
-  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+
+  mode: 'development',
 }
