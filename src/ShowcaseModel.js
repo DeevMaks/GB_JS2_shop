2507901c9394679306a87b8219f485c0d2fecb41
Allpion <slavkin.131@gmail.com>
2022-01-06 20:53:01 +1000

    Lesson 3. Added Clases

---
+++ src/ShowcaseModel.js
@@ -0,0 +1,26 @@
+import ProductList from './ProductList.js'
+
+export default class ShowcaseModel extends ProductList{
+  constructor(apiHandler, eventEmmiter, cart) {
+    super([])
+    this.api = apiHandler
+    this.cart = cart
+    this.eventEmmiter = eventEmmiter
+  }
+
+  fetch(onError) {
+    this.api.getCatalog(
+      (data) => { 
+        this.list = JSON.parse(data)
+        this.eventEmmiter.emit('showcaseFeched', this.list)
+      },
+      onError
+    )
+  }
+
+  buy(id, onError) {
+    const product = this.find(id) 
+    if(product) cart.add(product, onError)
+  }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-import getProductList from "./mock/data.js";
-import renderGoodsList from "./showcase.js";
-import { send } from "./utils.js";
+import ApiHandler from './ApiHandler.js';
+import CartModel from './CartModel.js';
+import ShowcaseModel from './ShowcaseModel.js';
+import EventEmitter from './EventEmitter.js'
 import './styles/style.css'
 import './styles/style.scss'
 
 const API_URL = 'http://localhost:3000/api/v1'
-let productList = [];
-let cart = [];
 
-send((error) => { console.log(err) }, (res) => {
-    let list = JSON.parse(res);
-    productList = list;
-    renderGoodsList(productList);
-}, `${API_URL}/catalog`)
+const api = new ApiHandler(API_URL)
+const eventEmmiter = new EventEmitter()
 
-let buyed = {id: 5, title: "new", price: 999};
-send((error) => { console.log(err) }, (res) => {
-    cart.push(buyed)
-}, `${API_URL}/cart`, 'POST', JSON.stringify(buyed), {"Content-Type": "application/json"})
+const cart = new CartModel(api, eventEmmiter)
+const showcase = new ShowcaseModel(api, eventEmmiter, cart)
+
+eventEmmiter.subscribe('showcaseFeched', (data) => {
+    console.log(data)
+})
+
+eventEmmiter.subscribe('cartFeched', (data) => {
+    console.log(data)
+})
+
+showcase.fetch()
+cart.fetch()
