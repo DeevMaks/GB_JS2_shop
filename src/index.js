6e4b7b2ad8e06e029ef0f9b796b28d8e2e2768c7
Lomakin Sergey <sergeylomakinals@mail.ru>
2022-01-16 18:10:37 +0500

    lesson3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-import ApiHandler from "./ApiHandler";
+import ApiHandler from './ApiHandler';
+import CartModel from './CartModel';
+import ShowcaseModel from './ShowcaseModel';
+import EventEmitter from './EventEmitter';
 
 const API_URL = 'http://localhost:3000/api/v1';
 const api = new ApiHandler(API_URL);
+const eventEmmiter = new EventEmitter();
+const cart = new CartModel(api, eventEmmiter);
+const showcase = new ShowcaseModel(api, eventEmmiter, cart);
+
+eventEmmiter.subscribe('showcaseFeched', (data) => {
+  console.log(data)
+})
+
+eventEmmiter.subscribe('cartFeched', (data) => {
+  console.log(data)
+})
+
+showcase.fetch()
+cart.fetch()
